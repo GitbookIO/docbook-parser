bd06c8c687d7ea482249c6bc8e647c5caad1de2d
jpreynat <johan.preynat@gmail.com>
2016-01-11 15:04:49 +0100

    Add extractLegalNotice()

--- docbook.js
+++ docbook.js
@@ -67,6 +67,28 @@ Docbook.prototype.getInfo = function() {
     // Set Docbook main informations
     this.title = _.find(this.informations.children, 'name', 'title').children[0].data;
     this.subtitle = _.find(this.informations.children, 'name', 'subtitle').children[0].data;
+
+    // Set legal notice by extracting each <para> elements text
+    this.extractLegalNotice();
+};
+
+Docbook.prototype.extractLegalNotice = function() {
+    var legalNoticeElement = _.find(this.informations.children, 'name', 'legalnotice');
+
+    this.legalNotice = _.chain(legalNoticeElement.children)
+        .filter(function(element) {
+            var paragraphs = ['para', 'simpara', 'formalpara'];
+            return _.includes(paragraphs, element.name);
+        })
+        .map(function(element) {
+            return _.flatten(element.children, true)[0];
+        })
+        .map(function(element) {
+            if (element.type !== 'text') return '';
+            return element.data.replace(/[\t\n\s]+/g, ' ');
+        })
+        .value()
+        .join('\n');
 };
 
 Docbook.prototype.parse = function() {
